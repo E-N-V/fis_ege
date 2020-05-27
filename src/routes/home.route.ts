b2845cdeedb8cb2a97e5a9330d27a50a171fbfbb
Egor <39993671+S1ither@users.noreply.github.com>
2020-05-27 21:30:18 +0300

    fix

--- src/routes/home.route.ts
+++ src/routes/home.route.ts
@@ -1,11 +1,11 @@
 import {Router} from "express"
 const route = Router()
 
-import { HomeGET, HomeGETAdd, HomePOSTAdd } from "../controllers/Home.controller"
+import { HomeGET, HomeGETAdd, HomePOSTAdd, HomeGETAuth } from "../controllers/Home.controller"
 
 route.get(["/", "/home", "/index", "/index.html", "/home.html"], HomeGET)
 route.get("/add", HomeGETAdd)
-route.get("/auth", )
+route.get("/auth", HomeGETAuth)
 route.post("/add", HomePOSTAdd)
 
 export default route
